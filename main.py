a3ec824d5763e15f4eec304fc1e19a6a5d6636dd
RyanJKK <163304433+RyanJKK@users.noreply.github.com>
2025-09-16 20:47:32 -0300

    second-commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class MeuApp(App):
     def build(self):
-        return Label(text='BatePonto')
+        return Label(text='BatePontoo')
 
 if __name__ == '__main__':
     MeuApp().run()
